067e7d6283efcfbce3be70f8218ca0384d4d3fa3
prashanthsb01 <sbprashanth.aws@gmail.com>
2025-12-17 13:31:46 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
   });
 
 app.get('/profile-picture', function (req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/Prashanth.jpg"));
+  let img = fs.readFileSync(path.join(__dirname, "images/Prashanth.jpeg"));
   res.writeHead(200, {'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
