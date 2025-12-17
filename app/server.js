0954dc101f2a91e3eb505dc7209c1bc55256e215
prashanthsb01 <sbprashanth.aws@gmail.com>
2025-12-17 12:43:55 +0530

    Update user profile

--- app/server.js
+++ app/server.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
   });
 
 app.get('/profile-picture', function (req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
+  let img = fs.readFileSync(path.join(__dirname, "images/yashas_atreya-20251003-0001.jpg"));
   res.writeHead(200, {'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
